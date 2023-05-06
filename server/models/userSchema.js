847d068c3140356f71d151f56d1f5fefe09d589c
aditya6700 <dsaiaditya@gmail.com>
2023-05-06 15:13:05 +0530

    userSchema created

---
+++ server/models/userSchema.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+mongoose.pluralize(null);
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    cpassword: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    phone: {
+        type: Number,
+        require: true,
+        trim: true
+    },
+    work: {
+        type: String,
+        require: true,
+        trim: true
+    },
+});
+
+const Users = mongoose.model('User', userSchema);
+
+module.exports = Users;
