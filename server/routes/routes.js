6c44c77904f256a52dc60b3e29c2de6a3982d95c
aditya6700 <dsaiaditya@gmail.com>
2023-05-06 17:40:12 +0530

    configured login route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,46 +9,67 @@ router.get('/about', middleware, (req, res) => {
     console.log('middleware allowed about page to render'),
     res.status(201).send('about page');
 });
-router.get('/login', (req, res) => {
-    res.status(201).send('login page');
-});
-router.get('/register', (req, res) => {
-    res.status(201).send('register page');
+router.post('/login', async (req, res) => {
+    console.log(req.body);
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).json({ message: "enter both email and passowrd" });
+    }
+
+    try {
+        
+        const userFound = await Users.findOne({ email });
+        if (!userFound || userFound.password !== password) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        res.status(200).json({ message: 'login success' });
+
+    } catch (err) {
+        res.status(422).json({
+            message: 'unknown error',
+            error: err
+        });
+    }
 });
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
+router.post('/register', async (req, res) => {
+    
     const { username, email, phone, work, password, cpassword } = req.body;
 
     if (!username || !email || !phone || !work || !password || !cpassword) {
         return res.status(422).json({ message: "please fill all fields" });
     }
+    
+    try {
 
-    Users.findOne({ email })
-        .then((userFound) => {
-            if (userFound) {
-                return res.status(422).json({ error: 'A user already exists with same email' });
+        const userFound = await Users.findOne({ email });
+        if(userFound) {
+            return res.status(422).json({ error: 'A user already exists with same email' });
+        }
+
+        const user = new Users({ username, email, phone, work, password, cpassword });
+        try {
+            const userCreated = await user.save();
+            if (userCreated) {
+                res.status(201).json({
+                    message: 'user created successfully',
+                    data: userCreated
+                });
             }
-
-            const user = new Users({ username, email, phone, work, password, cpassword });
-
-            user.save()
-                .then(() => {
-                    res.status(201).json({ message: 'user created successfully' });
-                })
-                .catch((err) => {
-                    res.status(500).json({
-                        message: 'failed to create user',
-                        error: err
-                    });
-                })
-        })
-        .catch((err) => {
-            res.status(422).json({
-                message: 'unknown error',
+        } catch (err) {
+            res.status(500).json({
+                message: 'failed to create user',
                 error: err
             });
+        }
+
+    } catch (err) {
+        res.status(422).json({
+            message: 'unknown error',
+            error: err
         });
+    }
 });
 
 
