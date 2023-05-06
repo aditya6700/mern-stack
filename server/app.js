d2dcc7ee861ba1f4cc0752911cac606b3d2b6e43
aditya6700 <dsaiaditya@gmail.com>
2023-05-06 14:46:45 +0530

    connected to mongodb atlas

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const port = process.env.PORT || 1432;
+const DB = 'mongodb+srv://aditya:masterootMern@mern.rtua7hm.mongodb.net/mernstack?retryWrites=true&w=majority';
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('connection successful with the database');
+}).catch((err) => {
+    console.log('error connecting with the database: ', err);
+});
 
 const middleware = (req, res, next) => {
     console.log("middleware is active");
