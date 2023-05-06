e123a7234633aa8881843e7f62f510540205669d
aditya6700 <dsaiaditya@gmail.com>
2023-05-06 14:18:23 +0530

    sample middleware added

--- server/app.js
+++ server/app.js
@@ -4,13 +4,18 @@ const app = express();
 
 const port = process.env.PORT || 1432;
 
+const middleware = (req, res, next) => {
+    console.log("middleware is active");
+    next(); // calls the next step (render the page)
+}
+
 app.get('/', (req, res) => {
     res.status(201).send('hi there');
 });
-app.get('/home', (req, res) => {
-    res.status(201).send('home page');
-});
-app.get('/about', (req, res) => {
+
+// include middleware function b/w route and function so that middleware runs first and then the page renders
+app.get('/about', middleware, (req, res) => {
+    console.log('middleware allowed about page to render'),
     res.status(201).send('about page');
 });
 app.get('/login', (req, res) => {
